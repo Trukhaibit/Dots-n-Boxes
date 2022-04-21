3535626f4d568378af6d0cf4744ec305e19496b7
Trukhaibit <70858117+Trukhaibit@users.noreply.github.com>
2022-04-21 11:35:19 -0400

    Add files via upload

---
+++ dotsnboxes/dotsnboxes/Page2.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace dotsnboxes
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        int turn = 3;
+        int counter = 0;
+        int inplay = 0;
+        public Page2(int players)
+        {
+            InitializeComponent();
+            inplay = players;
+        }
+
+        private void dot_Click(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse dot = e.Source as Ellipse;
+            dot.Fill = Brushes.Black;
+            if (counter == 0)
+            {
+                text.Text = dot.Name.Trim('_');
+                counter += 1;
+            }
+            else if (counter == 1)
+            {
+                int lineName = (int.Parse(dot.Name.Trim('_')) + int.Parse(text.Text)) / 2;
+                Rectangle line = (Rectangle)FindName("_" + lineName);
+                line.Fill = Brushes.Black;
+                counter -= 1;
+            }
+        }
+    }
+}
